ba9d0e30fe264acad6a6313f92d30e0aac68cbaa
Cristian Heitt <agustin_cristian.heitt@hotmail.com>
2022-12-01 18:04:45 -0300

    Solve Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,48 @@
 fun main() {
+
     fun part1(input: List<String>): Int {
-        return input.size
+        var maxAccumulated = 0
+        var accumulated = 0
+
+        for (calories in input) {
+            if (calories.isEmpty()) {
+                accumulated = 0
+                continue
+            }
+            val value = calories.toInt()
+            accumulated += value
+
+            if (accumulated > maxAccumulated)
+                maxAccumulated = accumulated
+        }
+
+        return maxAccumulated
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var accumulated = 0
+        var listOfTotals = mutableListOf<Int>()
+
+        for (calories in input) {
+            if (calories.isEmpty()) {
+                listOfTotals.add(accumulated)
+                accumulated = 0
+                continue
+            }
+            val value = calories.toInt()
+            accumulated += value
+        }
+        listOfTotals.add(accumulated)
+        return listOfTotals.sortedDescending().subList(0,3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
+
 }
